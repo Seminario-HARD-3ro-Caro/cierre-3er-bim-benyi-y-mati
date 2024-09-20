f961635415f5634f319df13b7dca3be4b026c48c
Bensagra <bensagra@gmail.com>
2024-09-20 10:40:03 -0300

    Refactor code to control LEDs and button functionality

--- 2_Sagranichne_Ojman.ino
+++ 2_Sagranichne_Ojman.ino
@@ -1,3 +1,32 @@
+// Ejercicio evaluatorio 3er bimestre
+/*
+  Integrantes
+    Nombre: Benyamin
+    Apellido: Sagranichne
+    Curso: 3A
+    
+    Nombre: Matias
+    Apellido: Ojman
+    Curso: 3A
+  
+Congisgnas
+  Conexionado:
+    En la protoboard, conectar 4 LEDs para que cada uno pueda ser controlado individualmente. Cada LED deberá tener su respectiva resistencia.
+    Se debe conectar tambien, un pulsador que permita recibir información de un usuario.
+    Indicar a continuación los pines a los cuales se conectó cada elemento y el valor de resistencias utilizado:
+      Pines:
+        Boton: 2
+        LED1: 3
+        LED2:4
+        LED3:5
+        LED4:6
+      Valor:
+        R_boton: 47k
+        R_LEDs: 330
+*/
+
+//Definimos todas nuestras variables y asignamos valores de cada pin
+
 #define azul 3
 #define verde 6
 #define rojo 4
@@ -12,6 +41,8 @@ boolean press = false;
 
 
 void setup() {
+      //Asignamos si va a ser input o output cada elemento
+
   pinMode(azul, OUTPUT);
   pinMode(verde, OUTPUT);
   pinMode(rojo, OUTPUT);
@@ -19,32 +50,29 @@ void setup() {
     pinMode(amarillo, OUTPUT);
 pinMode(boton, INPUT);
 
-  Serial.begin(9600);
 }
 
 void loop() {
   
-  // Leer datos del puerto serial solo si hay datos disponibles
   
 
-  // Condiciones para el color VERDE
+  // Condiciones para el color Azul
   if (digitalRead(boton) == 1 && verde_bool && !press) {
+    //Bandera para evitar múltiples ejecuciones con el mismo valor
     press = true;
-    Serial.println("azul");
     azul_bool = true;
     rojo_bool = false;
 
         amarillo_bool = false;
 verde_bool = false;
-
+    //encender el LED correspondiente al color Azul
     digitalWrite(azul, HIGH);
     digitalWrite(rojo, LOW);
     digitalWrite(verde, LOW);
     digitalWrite(amarillo, LOW);
     
-    // Reiniciar data para evitar múltiples ejecuciones con el mismo valor
   }
-  // Condiciones para el color AZUL
+    // Condiciones para el color Rojo
  else if (digitalRead(boton) == 1 && azul_bool && !press) {
     press = true;
     Serial.println("azul");
@@ -53,44 +81,41 @@ verde_bool = false;
 
         amarillo_bool = false;
 verde_bool = false;
-
+    //Encender el LED correspondiente al color Rojo
     digitalWrite(azul, LOW);
     digitalWrite(rojo, HIGH);
     digitalWrite(verde, LOW);
     digitalWrite(amarillo, LOW);
     
-    // Reiniciar data para evitar múltiples ejecuciones con el mismo valor
   }
 else   if (digitalRead(boton) == 1 && rojo_bool && !press) {
     press = true;
-    Serial.println("azul");
     azul_bool = false;
     rojo_bool = false;
 
         amarillo_bool = true;
 verde_bool = false;
-
+    // Encender el LED correspondiente al color Amarillo
     digitalWrite(azul, LOW);
     digitalWrite(rojo, LOW);
     digitalWrite(verde, LOW);
     digitalWrite(amarillo, HIGH);
     
-    // Reiniciar data para evitar múltiples ejecuciones con el mismo valor
   }
    else if (digitalRead(boton) == 1 && amarillo_bool && !press) {
+    //Bandera para evitar múltiples ejecuciones con el mismo valor
     press = true;
     azul_bool = false;
     rojo_bool = false;
 
         amarillo_bool = false;
 verde_bool = true;
-
+    // Encender el LED correspondiente al color Azul
     digitalWrite(azul, LOW);
     digitalWrite(rojo, LOW);
     digitalWrite(verde, HIGH);
     digitalWrite(amarillo, LOW);
     
-    // Reiniciar data para evitar múltiples ejecuciones con el mismo valor
   }
 
 
